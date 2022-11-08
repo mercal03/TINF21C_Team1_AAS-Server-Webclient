d5e0bde8eb476353b79420efc4db650a0fea7169
mercal03 <marcel.hintze@web.de>
2022-11-08 15:34:56 +0100

    some changes

--- SOURCE/aas-webclient/src/mainpage.js
+++ SOURCE/aas-webclient/src/mainpage.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 let serverUrl = "http://localhost:51310";
+let aasIDs = [];
 
 function request() {
     let request = new XMLHttpRequest();
@@ -10,7 +11,18 @@ function request() {
     request.onreadystatechange = () => {
         if (request.status === 200 && request.readyState === 4) {
             let json = JSON.parse(request.responseText);
-            console.log(json.aaslist[0]);
+
+            for (let i = 0; i < json.aaslist.length; i++) {
+                let id = json.aaslist[i];
+                let indexes = [];
+                for (let x = 0; x < id.length; x++) {
+                    if (id[x] === ':') {
+                        indexes.push(x);
+                    }
+                }
+                aasIDs.push(id.slice(indexes[0] + 2, indexes[1] - 1));
+            }
+            console.log(aasIDs);
         }
     }
 }

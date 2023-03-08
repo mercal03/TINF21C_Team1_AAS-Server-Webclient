c54bab1bb6d3ed0c24634eaf1b1760658cd4aa6e
Severin <Sevi2510@gmail.com>
2023-03-08 15:06:05 +0100

    Implemnetierung Suchleiste mit Suchfunktion nach AAS-Name

--- SOURCE/aas-webclient/src/filter.js
+++ SOURCE/aas-webclient/src/filter.js
@@ -1,13 +1,37 @@
 import React from "react";
+import {shells} from "./backend";
+import button from "bootstrap/js/src/button";
 
 class Filter extends React.Component {
-  render() {
-    return (
-        <div className="bg-primary-subtle w-25 p-2">
-            <h3>Filters</h3>
-        </div>
-    );
-  }
+
+    filterForName() { //Sucht nach einem AAS name
+        let searchInput = document.getElementById('searchField').value.toLowerCase(); // zeiht sich den Namen aus dem Inputfeld
+        let newAssetArray = [];
+
+        shells.forEach(element => {
+                if (element[0].toLowerCase().search(searchInput) !== -1) { // Abfrage ob Suchstring enthalten ist
+                    newAssetArray.push(element);
+                }
+            }
+        );
+        console.log(newAssetArray);
+    }
+
+
+    render() {
+        return (
+            <div className="bg-primary-subtle w-25 p-2">
+                <h3>Filters</h3>
+                {/* Suchfeldleiste */}
+                <input id={"searchField"} type={"text"} placeholder={"Search"}/>
+                <button class={"btn btn-primary"} onClick={this.filterForName}> Search</button>
+
+                {/* filter optionen */}
+
+            </div>
+        );
+    }
+
 }
 
 export default Filter;

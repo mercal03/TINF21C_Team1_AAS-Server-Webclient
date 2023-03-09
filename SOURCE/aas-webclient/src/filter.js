d3c8f24b575c02e58c8e98887b8edf66d5361856
Severin1112 <Sevi2510@gmail.com>
2023-03-09 16:09:16 +0100

    Fix: Suchfeld fängt leere Rückgabe ab.

--- SOURCE/aas-webclient/src/filter.js
+++ SOURCE/aas-webclient/src/filter.js
@@ -5,7 +5,7 @@ import button from "bootstrap/js/src/button";
 class Filter extends React.Component {
 
     filterForName() { //Sucht nach einem AAS name
-        let searchInput = document.getElementById('searchField').value.toLowerCase(); // zeiht sich den Namen aus dem Inputfeld
+        let searchInput = document.getElementById('searchField').value.toLowerCase();// zieht sich den Namen aus dem Inputfeld
         let newAssetArray = [];
 
         shells.forEach(element => {
@@ -14,7 +14,13 @@ class Filter extends React.Component {
                 }
             }
         );
-        console.log(newAssetArray);
+        if(newAssetArray.length === 0){
+            alert("No results found");
+        }
+        else {
+            console.log(newAssetArray);
+            alert(newAssetArray);
+        }
     }
 
 
@@ -25,7 +31,7 @@ class Filter extends React.Component {
                 <h3>Suche:</h3>
                 {/* Suchfeldleiste */}
                 <form class="form-inline d-md-flex">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <input id="searchField" className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
                     <button class="btn btn-primary my-2 my-sm-0" type="submit" onClick={this.filterForName}>Search</button>
                 </form>
                 <h3>Filter:</h3>

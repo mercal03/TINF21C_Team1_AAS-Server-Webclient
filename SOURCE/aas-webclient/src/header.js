c6e2b6e4f82591be2240b98b0e06d70cc2e25a6a
Anja Niedermeier <67066312+AnjaNiedermeier@users.noreply.github.com>
2023-03-13 14:22:21 +0100

    add server-menu

--- SOURCE/aas-webclient/src/header.js
+++ SOURCE/aas-webclient/src/header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {getFullShellData} from "./backend";
 import {index, Main} from "./index";
+import ServerMenu from "./serverMenu";
 
 class Header extends React.Component {
 
@@ -23,21 +24,12 @@ class Header extends React.Component {
                     <div className="d-flex flex-wrap align-items-center justify-content-md-between justify-content-center">
                         <h1 className="mx-2">AAS-Webclient</h1>
                         <div className="d-flex flex-nowrap align-items-center justify-content-between">
-                            <form className="mx-2" onSubmit={event => event.preventDefault()}>
-                                <input
-                                    id="server-url"
-                                    type="text"
-                                    className="form-control form-control-dark"
-                                    placeholder="Type in Server-URL...">
-                                </input>
-                                <button type="submit" className="btn btn-outline-light mx-2" onClick={this.addServer}>
-                                    Add Server
-                                </button>
-                            </form>
-                        </div>
                         <button onClick={this.clear}>{/* Wird wieder entfernt dient nur zum Testen*/}
                             Clear
                         </button>
+                       <ServerMenu/>
+                        </div>
+                        
                     </div>
                 </div>
             </header>
---
+++ SOURCE/aas-webclient/src/serverMenu.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { getFullShellData } from "./backend";
+import { index, Main } from "./index";
+import Dropdown from 'react-bootstrap/Dropdown';
+
+class ServerMenu extends React.Component {
+  addServer() {
+    index.render(<Main />);
+    let url = document.getElementById("server-url").value;
+    window.sessionStorage.setItem("url", url);
+    getFullShellData();
+  }
+
+  clear() {
+    window.sessionStorage.clear();
+    index.render(<Main />);
+  }
+
+  render() {
+    return (
+        <Dropdown autoClose="outside" variant="light">
+          <Dropdown.Toggle  id="dropdown-autoclose-outside">
+            Server Menü
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item><form
+                className="mx-2 d-flex flex-column"
+                onSubmit={(event) => event.preventDefault()}
+              >
+                <input
+                  id="server-url"
+                  type="text"
+                  className="form-control form-control-dark  w-auto"
+                  placeholder="Type in Server-URL..."
+                ></input>
+                <button
+                  type="submit"
+                  className="btn btn-link mx-2 text-nowrap"
+                  onClick={this.addServer}
+                >
+                  Add Server
+                </button>
+              </form></Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Example Server</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      );
+  }
+}
+
+export default ServerMenu;
+
+<div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown button
+        </button>
+        <div class="dropdown-menu">
+              
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">
+                Example Server A
+              </a>
+              <a class="dropdown-item" href="#">
+                Example Server B
+              </a>
+            </div>
+      </div>

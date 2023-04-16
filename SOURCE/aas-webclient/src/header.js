f74847975dd9ec84a9d251fea0448fd73206b23a
Anja Niedermeier <67066312+AnjaNiedermeier@users.noreply.github.com>
2023-04-16 17:11:07 +0200

    enhance itemview

--- SOURCE/aas-webclient/src/header.js
+++ SOURCE/aas-webclient/src/header.js
@@ -5,17 +5,16 @@ import Filter from "./filter"
 class Header extends React.Component {
     render() {
         return (
-            <header id="header" className="p-3 bg-primary text-white position-fixed w-100">
-                <div className="container">
+            <header id="header" className="p-2 bg-primary text-white sticky-top shadow-sm w-100">
                     <div className="d-flex flex-wrap align-items-center justify-content-md-between justify-content-center">
-                        <h1 className="mx-2">AAS-Webclient</h1>
+                        <div><h1>AAS-Webclient<span className="version">v.1.3</span></h1></div>
+                        
                         <div className="d-flex flex-nowrap align-items-center justify-content-between">
                             <Filter/>
                             <ServerMenu/>
                         </div>
                         
                     </div>
-                </div>
             </header>
         );
     }

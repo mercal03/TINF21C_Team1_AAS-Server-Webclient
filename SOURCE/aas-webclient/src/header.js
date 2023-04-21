bb050464c2a8241703cd8e0ee431160fa68f992f
mercal03 <marcel.hintze@web.de>
2023-04-21 09:51:27 +0200

    Added current Server view dings

--- SOURCE/aas-webclient/src/header.js
+++ SOURCE/aas-webclient/src/header.js
@@ -7,8 +7,8 @@ class Header extends React.Component {
             <header id="header" className="py-2 px-3 bg-primary text-white sticky-top shadow-sm w-100">
                     <div className="d-flex flex-wrap align-items-center justify-content-md-between justify-content-center">
                         <div><h1>AAS-Webclient</h1></div>
-                        
                         <div className="d-flex flex-nowrap align-items-center justify-content-between">
+                            <p>Current Server: {window.sessionStorage.getItem("url") ? window.sessionStorage.getItem("url") : ""}</p>
                             <ServerMenu/>
                         </div>
                         
